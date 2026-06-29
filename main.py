ee8971e572e7aac17293377b939e0b5d677e28f5
falaahenergco-dev <falaah.energco@gmail.com>
2026-06-29 16:31:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
 """
+import os
+from threading import Thread
+from flask import Flask
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot is running!"
+
+def run():
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
 بوت تليغرام لإدارة محل الكوزمتك - نسخة Render (24/7 مجاني)
 """
 
